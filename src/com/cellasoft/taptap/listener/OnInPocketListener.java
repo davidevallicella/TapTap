7454ccf69ddf191884f32176b0a4ce4219bb4480
Davide Vallicella <davide.vallicella@netsysco.net>
2015-03-21 00:05:17 +0100

    Version 0.1.2

---
+++ src/com/cellasoft/taptap/listener/OnInPocketListener.java
@@ -0,0 +1,11 @@
+package com.cellasoft.taptap.listener;
+
+/**
+ * Interface for in-pocket gesture.
+ */
+public interface OnInPocketListener {
+    /**
+     * Called when in-pocket is detected.
+     */
+    void onInPocket();
+}
